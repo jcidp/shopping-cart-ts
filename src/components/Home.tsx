7e409ab68a774a19ff031585c13b7ab356c71516
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2024-01-14 19:13:53 -0600

    Add content to Home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,16 @@
-const Home = () => {
-  return (
-    <h2>Hello, world!</h2>
-  )
-}
+import { Link } from "react-router-dom";
+import styles from "../styles/Home.module.css";
 
-export default Home
+const Home = () => {
+    return (
+        <section className={styles.heroSection}>
+            <div className={styles.heroText}>
+                <h2>The store with things you don't really want!</h2>
+                <p>Add an item to your cart and go to the checkout to "buy" it ;)</p>
+                <Link className={styles.btnPrimary} to="/shop">Shop Now</Link>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
